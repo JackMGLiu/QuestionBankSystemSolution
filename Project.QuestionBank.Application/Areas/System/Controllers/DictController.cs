c24d282b4727bf57781cc6716aca622584b31f58
Jack-Liu <love_zs1314@126.com>
2018-11-03 18:03:28 +0800

    123

---
+++ Project.QuestionBank.Application/Areas/System/Controllers/DictController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.QuestionBank.Application.Areas.System.Controllers
+{
+    public class DictController : Controller
+    {
+        // GET: System/Dict
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
