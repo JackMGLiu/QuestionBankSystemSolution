2b62679ff1a56be3c5798c62e62614f45d38f974
Jack-Liu <love_zs1314@126.com>
2018-11-04 00:44:19 +0800

    完成了ui样式及自定义title

--- Project.QuestionBank.Application/Controllers/HomeController.cs
+++ Project.QuestionBank.Application/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project.QuestionBank.Utils.Helper;
 
 namespace Project.QuestionBank.Application.Controllers
 {
@@ -10,6 +11,8 @@ namespace Project.QuestionBank.Application.Controllers
     {
         public ActionResult Index()
         {
+            string title = ConfigHelper.GetConfig("SystemName");
+            ViewBag.SystemName = title;
             return View();
         }
 
